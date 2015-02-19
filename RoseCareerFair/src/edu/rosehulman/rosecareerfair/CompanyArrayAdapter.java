6f360be9308bf6935170ea99c86b027e7d7adad6
Joe Malik <azwildcat3@comcast.net>
2015-02-18 22:06:19 -0500

    newCode
    Its not broken!

--- RoseCareerFair/src/edu/rosehulman/rosecareerfair/CompanyArrayAdapter.java
+++ RoseCareerFair/src/edu/rosehulman/rosecareerfair/CompanyArrayAdapter.java
@@ -16,34 +16,37 @@ import android.widget.ToggleButton;
 public class CompanyArrayAdapter extends ArrayAdapter<Company>{
 	
 	private ToggleButton mFavoriteButton;
+	private Context mContext;
 	
 	public CompanyArrayAdapter(Context context, int resource, int textViewResourceId, List<Company> companies) {
 		super(context, resource, textViewResourceId, companies);
+		mContext = context;
 	}
 	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View view = super.getView(position, convertView, parent);
 		TextView companyTextView = (TextView) view.findViewById(android.R.id.text1);
+		companyTextView.setBackgroundColor(mContext.getResources().getColor(R.color.gray));
 		companyTextView.setText(getItem(position).getName());
-		mFavoriteButton = (ToggleButton) view.findViewById(android.R.id.button1);
-		mFavoriteButton.setBackgroundColor(view.getResources().getColor(R.color.gray));
-		mFavoriteButton.setChecked(false);
-		mFavoriteButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				if(mFavoriteButton.isChecked()){
-					mFavoriteButton.setBackgroundColor(v.getResources().getColor(R.color.gray));
-					mFavoriteButton.setChecked(false);
-				}
-				else{
-					mFavoriteButton.setBackgroundColor(v.getResources().getColor(R.color.red));
-					mFavoriteButton.setChecked(true);
-				}
-				
-			}
-		});
+//		mFavoriteButton = (ToggleButton) mView.findViewById(android.R.id.button1);
+//		mFavoriteButton.setBackgroundColor(mView.getResources().getColor(R.color.gray));
+//		mFavoriteButton.setChecked(false);
+//		mFavoriteButton.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				if(mFavoriteButton.isChecked()){
+//					mFavoriteButton.setBackgroundColor(mView.getResources().getColor(R.color.gray));
+//					mFavoriteButton.setChecked(false);
+//				}
+//				else{
+//					mFavoriteButton.setBackgroundColor(mView.getResources().getColor(R.color.red));
+//					mFavoriteButton.setChecked(true);
+//				}
+//				
+//			}
+//		});
 		return view;
 	}
 }
